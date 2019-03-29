7f0e4ddd444031b8e5bd6c2b88c445a30b395905
Burak Calisgan <calisganburak@gmail.com>
2019-03-29 22:48:30 +0300

    Bilinen değerlerle test yapılması için menü ve ekran hazırlandı. AlertDialog düzenlendi.

--- app/src/main/java/com/example/trilateration/MainActivity.java
+++ app/src/main/java/com/example/trilateration/MainActivity.java
@@ -11,6 +11,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.util.SparseBooleanArray;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -121,6 +123,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             wifiList.add(results.get(i).SSID + "  (" + results.get(i).level + ")" + "*" +  results.get(i).frequency);
             adapter.notifyDataSetChanged();
         }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        else if (id == R.id.action_test_trilateration){
+            Intent intent = new Intent(getApplicationContext(),TestTrilateration.class);
+            startActivity(intent);
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
 
